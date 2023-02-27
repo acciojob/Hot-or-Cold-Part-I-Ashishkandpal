7e62f44c3c2c288222eb71aa323119c0577f4083
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-27 22:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const response = document.querySelector('#response');
 const random = Math.floor(Math.random() * (20 - 1 + 1) + 1);
 response.innerHtml = random;
 
+const checking = docuemnt.querySelector('.checking');
+checking.addEventListener('click', function (e) {
+	e.preventDefault();
+	
+})
