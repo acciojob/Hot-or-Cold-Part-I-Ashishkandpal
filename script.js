df6bf6512364c5a22e5dda48250c5b17c6fa02fc
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-27 23:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 //your code here
-const guessed = Number(document.querySelector('#guess').value);
-const response = document.querySelector('#respond');
+const response = document.querySelector("#respond");
 const random = Math.floor(Math.random() * (20 - 1 + 1) + 1);
 
-let numOfGuess = 0;
-const checking = docuemnt.querySelector('.checking');
-checking.addEventListener('click', function (e) {
-	e.preventDefault();
-	if(numOfGuess < 2) {
-		if(guessed < random) {
-			response.textContent = 'guess higher';
-		}
-		else {
-			response.textContent = 'guess lower';
-		}
-	} else {
-		let diff = guessed - random;
-		if(diff < prevDiff) {
-			response.textContent = 'getting hotter';
-		}
-		else if(guessed < random) {
-			response.textContent = 'guess higher';
-		}
-		else {
-			response.textContent = 'guess lower';
-		}
-	}
-	let prevDiff = guessed - random;
-	numOfGuess++;
-})
+let numOfGuess = 1;
+let prevDiff = 0;
+
+const checking = document.querySelector(".checking");
+
+
+checking.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guessed = document.querySelector("#guess").value;
+  if (numOfGuess < 2) {
+    if (guessed < random) {
+      response.textContent = "guess higher";
+    } else if (guessed > random) {
+      response.textContent = "guess lower";
+    }
+  } else {
+    let diff = guessed - random;
+    if (diff < prevDiff) {
+      response.textContent = "getting hotter";
+    } else if (guessed < random) {
+      response.textContent = "guess higher";
+    } else if (guessed > random) {
+      response.textContent = "guess lower";
+    }
+  }
+  prevDiff = guessed - random;
+  numOfGuess++;
+});
