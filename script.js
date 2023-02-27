99e864139ba9b1c0e4e07c1262e11016e8bff9e1
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-27 22:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //your code here
+const guessed = Number(document.querySelector('#response').value);
 const response = document.querySelector('#response');
 const random = Math.floor(Math.random() * (20 - 1 + 1) + 1);
 response.innerHtml = random;
 
+let numOfGuess = 0;
 const checking = docuemnt.querySelector('.checking');
 checking.addEventListener('click', function (e) {
 	e.preventDefault();
-	
+	if(numOfGuess < 2) {
+		if(guessed < random) {
+			response.textContent = 'guess higher';
+		}
+		else {
+			response.textContent = 'guess lower';
+		}
+	} else {
+		let diff = guessed - random;
+		if(diff < prevDiff) {
+			response.textContent = 'getting hotter';
+		}
+		else if(guessed < random) {
+			response.textContent = 'guess higher';
+		}
+		else {
+			response.textContent = 'guess lower';
+		}
+	}
+	let prevDiff = guessed - random;
+	numOfGuess++;
 })
